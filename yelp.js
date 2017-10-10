027e831e863c61a53a9428a64ab7935a87337428
ckim618 <ckim618@gmail.com>
2017-10-10 12:28:35 -0700

    got yelp api resonse to work, need to pass through long and lat and term from picture that is randomly selected

--- yelp.js
+++ yelp.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
+    var proxy = 'https://cors-anywhere.herokuapp.com/';
+    var yelpApi = 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=33.634858033&longitude=-117.7404640&Authorization=Bearer N6_WFXHCeAWzeFBJvljs8lgptMrgkJoakrMe8wiS04dihDrsNiFWu4rWc1_5X7HzcV-tbq9L2lUOQ5qPNYloCRoexh57VDFuaaVG7p3MnlQEQ1bG59HP3vqSoSLcWXYx';
     $.ajax({
         dataType: 'json',
-        url: 'https://api.yelp.com/v3/businesses/search',
-        Authorization: 'Bearer N6_WFXHCeAWzeFBJvljs8lgptMrgkJoakrMe8wiS04dihDrsNiFWu4rWc1_5X7HzcV-tbq9L2lUOQ5qPNYloCRoexh57VDFuaaVG7p3MnlQEQ1bG59HP3vqSoSLcWXYx',
+        method: 'get',
+        url: proxy + yelpApi,
+        headers: {Authorization: 'Bearer N6_WFXHCeAWzeFBJvljs8lgptMrgkJoakrMe8wiS04dihDrsNiFWu4rWc1_5X7HzcV-tbq9L2lUOQ5qPNYloCRoexh57VDFuaaVG7p3MnlQEQ1bG59HP3vqSoSLcWXYx'},
         term: 'delis',
         latitude: 33.634858033,
         longitude: 117.8265,
         success: function (response) {
-            console.log('Yelp response worked');
+            console.log('Yelp response worked', response);
         }
-
-
-    })
+    });
 });
