a806a8293c16b5d96fb4a8f124c9b8db4b1d5837
h2t2 <harrytran.w@gmail.com>
2017-10-10 14:58:35 -0700

    changes for result screen

--- yelp.js
+++ yelp.js
@@ -1,12 +1,11 @@
 
     function yelpCall() {
-    debugger;
         var proxy = 'https://cors-anywhere.herokuapp.com/';
         var yelpApi = 'https://api.yelp.com/v3/businesses/search?term=' + pickedCuisine.foodType + '&latitude=' + userLocation_result.lat +'&longitude=' + userLocation_result.lng +'&Authorization=Bearer N6_WFXHCeAWzeFBJvljs8lgptMrgkJoakrMe8wiS04dihDrsNiFWu4rWc1_5X7HzcV-tbq9L2lUOQ5qPNYloCRoexh57VDFuaaVG7p3MnlQEQ1bG59HP3vqSoSLcWXYx';
         $.ajax({
             dataType: 'json',
             method: 'get',
-            url: proxy + yelpApi,
+            url: yelpApi,
             headers: {Authorization: 'Bearer N6_WFXHCeAWzeFBJvljs8lgptMrgkJoakrMe8wiS04dihDrsNiFWu4rWc1_5X7HzcV-tbq9L2lUOQ5qPNYloCRoexh57VDFuaaVG7p3MnlQEQ1bG59HP3vqSoSLcWXYx'},
             success: function (response) {
                 console.log('Yelp response worked', response);
