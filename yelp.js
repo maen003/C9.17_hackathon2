f8189324f6516d1c8ccfa5dd9b0209c23fab8400
maen003 <maen003@gmail.com>
2017-10-11 15:48:32 -0700

    icon now yellow

--- yelp.js
+++ yelp.js
@@ -65,8 +65,6 @@ function addDescription(){
     });
     $('#yelpInfo').append($businessName,$businessPhone ,$businessAddress,$goToYelpButton);
     $('#food').attr('src',yelpPicture);
-    $('#yelpInfo').append(yelpName + ' ' +yelpAddress)
-
 }
 
 function directToYelp() {
