0ebc14901517e148fa1d477a664ac520e27f37e7
ckim618 <ckim618@gmail.com>
2017-10-12 10:08:27 -0700

    going to merge with latest changes

--- yelp.js
+++ yelp.js
@@ -2,6 +2,7 @@ var yelpName;
 var yelpPicture;
 var yelpAddress;
 var yelpInfo;
+
 function yelpCall() {
     var proxy = 'https://cors-anywhere.herokuapp.com/';
     var yelpApi = 'https://api.yelp.com/v3/businesses/search?term=' + pickedCuisine.foodType + '&latitude=' + userLocation_result.lat +'&longitude=' + userLocation_result.lng +'&Authorization=Bearer N6_WFXHCeAWzeFBJvljs8lgptMrgkJoakrMe8wiS04dihDrsNiFWu4rWc1_5X7HzcV-tbq9L2lUOQ5qPNYloCRoexh57VDFuaaVG7p3MnlQEQ1bG59HP3vqSoSLcWXYx';
